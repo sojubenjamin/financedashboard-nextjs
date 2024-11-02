cea05290a37cce4a1bae56817740c3dcea943e66
sojubenjamin <sojubenjamin@gmail.com>
2024-11-02 18:26:00 +0800

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter,Lusitana } from 'next/font/google';
+
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = ({
+weight: ['400','700'],
+subsets: ['latin'],
+
+
+});
